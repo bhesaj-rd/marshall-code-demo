c9d9529d43d3a4a0e0bb28087752cd06a390d181
Bhesaj <bhesaj444@gmail.com>
2026-02-24 22:15:04 +0900

    Add about page with project info and /about route

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 
@@ -9,8 +11,16 @@ const server = http.createServer((req, res) => {
     return;
   }
 
+  if (req.url === '/about') {
+    const aboutPath = path.join(__dirname, 'about.html');
+    const html = fs.readFileSync(aboutPath, 'utf8');
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(html);
+    return;
+  }
+
   res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.end('<h1>Marshall Code Demo</h1><p>Pipeline test project.</p>');
+  res.end('<h1>Marshall Code Demo</h1><p>Pipeline test project.</p><p><a href="/about">About</a></p>');
 });
 
 server.listen(PORT, () => {
